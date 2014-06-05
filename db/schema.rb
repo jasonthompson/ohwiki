c78108bb04fba98a9efe74af74105e89adb44189
Jason Thompson <jason@jthompson.ca>
2014-06-05 10:01:37 -0400

    After running migration at work

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140603192123) do
+ActiveRecord::Schema.define(version: 20140605105022) do
 
   create_table "pages", force: true do |t|
     t.text     "title"
@@ -35,4 +35,5 @@ ActiveRecord::Schema.define(version: 20140603192123) do
   end
 
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+
 end
