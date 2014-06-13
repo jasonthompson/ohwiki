168dc8090849ad00283dfdc91df3ccd111fcbf89
ohwiki <ohwiki@jthompson.ca>
2014-06-13 13:40:58 -0400

    Prepare for initial deployment
    -Using puma
    -Added Heroku's 'rails_serve_static_assets'
    -Precompiled static assets
    -Set up Postgresql
    -Created db/seeds to create temp admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.new(:email => 'admin@example.ca',
+         :password => 'secret',
+         :password_confirmation => 'secret',
+         :admin => "1").save
